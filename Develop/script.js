fe5f96ca33aade402c71accde5e532844647a34b
RyeDao <ryan.t.dao@gmail.com>
2022-09-11 21:55:00 -0700

    Testing script.js with current time (21:45H) to see if code works.

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,49 @@
 // Display today's date.
-
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+$(document).ready(function () {
+    // save button function
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+
+
+    })
+   
+    function timeTracker() {
+        var timeNow = moment().hour();
+
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    // local storage
+    $("#hour20 .description").val(localStorage.getItem("hour20"));
+    $("#hour21 .description").val(localStorage.getItem("hour21"));
+    $("#hour22 .description").val(localStorage.getItem("hour22"));
+    $("#hour23 .description").val(localStorage.getItem("hour23"));
+
+    timeTracker();
+})
